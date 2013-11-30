59cc7c05352becedf1104010793e3e71d02e473d
Gilles Legoux <gilles.legoux@ensimag.fr>
2013-11-30 12:05:52 +0100

    test

---
+++ projet_car/src/projet_car/Projet_car.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_car;
+
+/**
+ *
+ * @author gyls
+ */
+public class Projet_car {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
