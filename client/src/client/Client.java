a9a6d8cc966505e1841970925e24b988a5d663b2
Gilles Legoux <gilles.legoux@ensimag.fr>
2013-11-30 12:27:46 +0100

    test de la mise à jour d'un fichier par le git

--- client/src/client/Client.java
+++ client/src/client/Client.java
@@ -14,6 +14,6 @@ public class Client {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("je suis un 'client' !");
     }
 }
