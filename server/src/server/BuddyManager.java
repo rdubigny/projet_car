64088180d2674634f6d24c0ff1503d3a9e5d57f5
Raphaël Dubigny <rdubigny@gmail.com>
2013-11-30 19:28:01 +0100

    test: client-server communication over UDP

--- server/src/server/BuddyManager.java
+++ server/src/server/BuddyManager.java
@@ -4,23 +4,86 @@
  */
 package server;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 /**
  *
  * @author car team 16
  */
 public class BuddyManager extends Thread {
 
-    public BuddyManager() {
+    private final int portNumber = 4444;
+    private final InetAddress localAddr = null; // resolve into localhost;
+    private final DatagramSocket serverSocket; // UDP socket
+    
+    public BuddyManager() throws IOException {
+        // create socket
+        serverSocket = new DatagramSocket(portNumber, localAddr);
     }
 
     @Override
     public void run() {
-        while (true) {
+        // main loop
+        byte[] sendData;
+        // can receive 'Election', 'OK' or 'Coordinator'
+        byte[] receiveData = new byte[1024];
+        while(true){
             try {
-                System.out.println("BuddyManager: I'm alive");
-                BuddyManager.sleep(1000);
-            } catch (InterruptedException ex) {
+                DatagramPacket receivePacket =
+                        new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                String sentence = new String( receivePacket.getData());
+                System.out.println("RECEIVED: " + sentence);                
+                InetAddress IPAddress = receivePacket.getAddress();
+                int port = receivePacket.getPort();
+                String capitalizedSentence = sentence.toUpperCase();
+                sendData = capitalizedSentence.getBytes();
+                DatagramPacket sendPacket =
+                        new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                serverSocket.send(sendPacket);
+            } catch (IOException ex) {
+                System.out.println(ex.getStackTrace());
+            } finally {
+                serverSocket.disconnect();
             }
         }
     }
 }
+
+// TCP connexion example
+/*
+private final ServerSocket serverSocket;
+private final int backlog = 50; // number max of simultaneous connections
+serverSocket = new ServerSocket(portNumber, backlog, inetAddr);
+while (false) {
+    Socket clientSocket = null;
+    try {
+        clientSocket = serverSocket.accept();
+        System.out.println("BuddyManager: connexion received, "
+                +"opening a new connexion on port "
+                +clientSocket.getLocalPort());
+        PrintWriter clientOut =
+            new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader clientIn = new BufferedReader(
+            new InputStreamReader(clientSocket.getInputStream()));
+        clientOut.println("yo! welcome to this f*****g java server!");
+        String tmp;
+        while((tmp = clientIn.readLine()) != null){
+            System.out.println(tmp);
+        }
+    } catch (IOException ex) {
+        System.out.println(ex.getStackTrace());
+    } finally {
+        try {
+            clientSocket.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getStackTrace());
+            Logger.getLogger(BuddyManager.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+    }
+}
+*/
--- server/src/server/Server.java
+++ server/src/server/Server.java
@@ -4,6 +4,10 @@
  */
 package server;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author car team 16
@@ -14,14 +18,19 @@ public class Server {
      * @param args the argument will be about ip and port
      */
     public static void main(String[] args) {
-        // launch the main services as threads here
-        BuddyManager buddyManager = new BuddyManager();
-        buddyManager.start();
-        NameNodeManager nameNodeManager = new NameNodeManager();
-        nameNodeManager.start();
-        NameNode nameNode = new NameNode();
-        nameNode.start();
-        DataNodeManager dataNodeManager = new DataNodeManager();
-        DataNode dataNode = new DataNode();
+        try {
+            // launch the main services as threads here
+            BuddyManager buddyManager = new BuddyManager();
+            buddyManager.start();/*
+            NameNodeManager nameNodeManager = new NameNodeManager();
+            nameNodeManager.start();
+            NameNode nameNode = new NameNode();
+            nameNode.start();
+            DataNodeManager dataNodeManager = new DataNodeManager();
+            DataNode dataNode = new DataNode();*/
+        } catch (IOException ex) {
+            System.out.println(ex.getStackTrace());
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
