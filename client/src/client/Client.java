af51ccc12eef167cf92eb3966dd73149539fd122
Daniel Alves <danpaulalves@gmail.com>
2013-12-08 20:25:44 +0100

    Improving client connection.

--- client/src/client/Client.java
+++ client/src/client/Client.java
@@ -4,6 +4,7 @@ import data.*;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,27 +21,45 @@ public class Client {
         try {
             messenger = new Messenger(new Socket(host, port));
         } catch (IOException e) {
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, e);
+            System.out.println("Connection could not be established\n");
         }
     }
     
     /* method */
-    private static void run(String host, int port) {
+    private static void run(String host, int port) throws InterruptedException {
         System.out.println("\nWelcome to your shared file system!");
         DataContainer command = entryFormatter.getLogin();
         login = command.getDescription();
-        Client user = new Client(host, port);  
-        boolean connexionHasFinished = user.execute(command);
-        do {
-            System.out.print("> ");
-            command = entryFormatter.format();
-            if(command!=null) {
-                connexionHasFinished = user.execute(command);
-            } else {
-                System.out.println("Unknown command. Try: create, read, write, erase or quit.");
-            }             
-        } while (!connexionHasFinished);
-        user.close();
+        Client user = new Client(host, port);
+        int attempts = 0;
+        while(!user.isConnected() && attempts <= 5) {
+            attempts++;
+            System.out.println("Trying to connect to our servers.");
+            TimeUnit.SECONDS.sleep(5);
+            user = new Client(host, port);
+        }
+        
+        if (user.isConnected()) {
+            boolean connexionHasFinished = user.execute(command);
+            do {
+                System.out.print("> ");
+                command = entryFormatter.format();
+                if(command != null) {
+                    connexionHasFinished = user.execute(command);
+                } else 
+                    System.out.println("Unknown command. Try: create, read, write, erase or quit.");       
+            } while (!connexionHasFinished);
+            user.close();
+        }
+    }
+    
+    
+    /**
+     * 
+     * @return false if messenger could not be built, else true. 
+     */
+    private boolean isConnected() {
+        return messenger != null;
     }
     
     /**
@@ -82,6 +101,10 @@ public class Client {
     /* main */
     public static void main(String[] args) {
         while (true)
-            run("127.0.0.1", 1025);
+            try {
+                run("127.0.0.1", 1025);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
     }
 }
