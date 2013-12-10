d9a0dc323db2f565c2d14f5799bca79fc5893ba9
Raphaël Dubigny <rdubigny@gmail.com>
2013-12-10 13:02:15 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-### CONFIG ###
-
 # place where the directories of this script created
-# safety: don't erase a other existing directory
-DIR="$HOME/CAR_Application"
+# safety: don't erase a other directory
+DIR="$HOME/shared_filesystem"
 # N server machine(s)
-N=3
+N=4
 # M client machine(s)
-M=1
+M=0
 # P number of ports by machine
 P=3
 # parameters of config file
-CONFIG_FILE="server/config.txt"
+CONFIG_FILE_SERVER="server/config.txt"
+CONFIG_FILE_CLIENT="client/config.txt"
 # free port from 1024 to 65535
 NP=1024
 
 ### FUNCTION ###
 
 # line format in this file
+# config file server
 # idServer address buddyPort clientPort serverPort
-function create_config_file { 
+declare -a client_ports
+function create_config_file_server { 
     for i in $(seq 0 $((N-1))); do    
         echo -n "$i 127.0.0.1 "
         for j in $(seq 0 $((P-1))); do
+            if [ $j -eq 1 ]; then
+                client_ports[$i]=$NP;    
+            fi 
             echo -n "$NP "
             ((NP++))    
         done
@@ -31,67 +35,38 @@ function create_config_file {
     done
 }
 
-function execute_server {
-    cd "$DIR"/server"$i" > /dev/null
-    gnome-terminal -t "server $1" -e "java -jar server.jar $1"
-    cd - > /dev/null
-}
 
-function execute_client {
-    cd "$DIR"/client"$i" > /dev/null
-    gnome-terminal -t "client $1" -e "java -jar client.jar $1"
-    cd - > /dev/null
+# line format in this file
+# config file server
+# idServer address clientPort
+function create_config_file_client {
+    for i in $(seq 0 $((N-1))); do
+        echo "$i 127.0.0.1 ${client_ports[$i]}"
+    done     
 }
 
 ### SCRIPT ###
 
-# create the file config
-create_config_file > "$CONFIG_FILE"
+# create the config files
+echo Generating config file...
+create_config_file_server > "$CONFIG_FILE_SERVER"
+create_config_file_client > "$CONFIG_FILE_CLIENT"
+echo Generating config file...DONE
 
-# create the repertory contents all executables
 if [ ! -e "$DIR" ]; then
     mkdir "$DIR"
-else 
-    echo "ERREUR: $DIR exists !"
-    echo "Delete $DIR and create again (y/n) ?"
-    read response
-    if [ "$response" = 'y' ]; then
-        rm -rf "$DIR"
-        mkdir "$DIR"
-    else
-        exit 1
-    fi
 fi
 
-# create and launch the server in the previous repertory
-for i in $(seq 0 $((N-1))); do
-    mkdir -p "$DIR"/server"$i"
-    cp -r server/dist/* "$DIR"/server"$i"/
-    cp server/config.txt "$DIR"/server"$i"/
-    execute_server "$i"
-done
 
-
-# create and launch the client in the 
-for i in $(seq 0 $((M-1))); do
-    mkdir -p "$DIR"/client"$i"
-    cp -r client/dist/* "$DIR"/client"$i"/
-    cp server/config.txt "$DIR"/client"$i"
-    execute_client "$i"
-done
-
-# Track the done actions
-echo "$N server(s) deploy."
-echo "$M client(s) deploy."
+echo "$N server(s) to deploy"
 echo Deploying servers...
 
-
-# kill and recreate server
-INFINITY=true
-while $INFINITY; do
-    echo -n "create server : "
-    read number
-    execute_server "$number"
+# deploy the server
+for i in $(seq 0 $((N-1))); do
+    DEST="$DIR"/server"$i"
+    mkdir -p "$DEST"
+    cp -r server/dist/* "$DEST"/
+    cp server/config.txt "$DEST"/
 done
 
 echo Deploying servers...DONE
